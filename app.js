df4d248bc062a3c5e30104893883f0139909588f
VeetiSo <99474309+VeetiSo@users.noreply.github.com>
2022-11-13 19:38:33 +0200

    commit

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.set('views', './views');
-app.set('view engine', 'pug');
+app.set("views", "./views");
+app.set("view engine", "pug");
 
-app.get('/', (req, res) => {
-  res.render('home');
+app.use("/");
+
+app.get("/setCookie", (req, res, clr) => {
+  res.render("home");
 });
-
+app.get("/getCookie", (req, res) => {
+  res.render("home");
+});
+app.get("/deleteCookie", (req, res) => {
+  res.render("home");
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
