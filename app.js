e44266448f39cac7f7d4d86f007a91bbb9a64c08
VeetiSo <99474309+VeetiSo@users.noreply.github.com>
2022-11-25 10:00:07 +0200

    Did not push somewhy

--- app.js
+++ app.js
@@ -6,15 +6,27 @@ const port = 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.use("/");
+//app.use("/");
+if (typeof window !== "undefined") {
+  app.get("/setCookie/:clr", (req, res) => {
+    document.cookie = `color=${req.params.clr}`;
+    console.log("cookie added color " + req.params.clr);
+    res.json({
+      message: "cookie added",
+    });
+  });
+  app.get("/getCookie", (req, res) => {
+    console.log("cookie get color");
+    res.json({
+      message: "cookie get color" + document.cookie.color,
+    });
+  });
+  app.get("/deleteCookie", (req, res) => {
+    document.cookie = "color=";
+    res.json({
+      message: "cookie deleted",
+    });
+  });
+}
 
-app.get("/setCookie", (req, res, clr) => {
-  res.render("home");
-});
-app.get("/getCookie", (req, res) => {
-  res.render("home");
-});
-app.get("/deleteCookie", (req, res) => {
-  res.render("home");
-});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
